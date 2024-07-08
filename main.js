38ce738762299e3de0f6e2ba74feee96a676c8ec
farooqrimsha123 <159934322+farooqrimsha123@users.noreply.github.com>
2024-07-08 22:54:04 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let todos = [];
+let loop = true;
+while (loop) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "TODO",
+            message: "What do you want to add in your todo?",
+        },
+        {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo?",
+            default: false
+        },
+    ]);
+    const { TODO, addmore } = answers;
+    console.log(answers);
+    loop = addmore;
+    if ("TODO") {
+        todos.push("TODO");
+    }
+    else {
+        console.log("kindly add valid input");
+    }
+    console.log(todos);
+    if (todos.length > 0) {
+        console.log("tour Todo list: ");
+        todos.forEach(todo => {
+            console.log(todo);
+        });
+    }
+    else {
+        console.log("Not Todos Found");
+    }
+}
+;
